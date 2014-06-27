fcc423d1778492a80d391609b068aca69708a95d
Max Sharples <maxsharples@gmail.com>
2014-06-27 16:47:18 +1000

    added setup

---
+++ setup.py
@@ -0,0 +1,57 @@
+# Copyright 20014 Max Sharples
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    from setuptools import setup
+    # arguments that distutils doesn't understand
+    setuptools_kwargs = {
+        'install_requires': [
+            'boto>=2.2.0'
+        ],
+        'provides': ['apiarist']
+    }
+except ImportError:
+    from distutils.core import setup
+    setuptools_kwargs = {}
+
+import apiarist
+
+setup(
+    author='Max Sharples',
+    author_email='maxsharples@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Distributed Computing',
+    ],
+    description='Python Hive query framework',
+    license='Apache',
+    long_description=open('README.md').read(),
+    name='apiarist',
+    packages=[
+        'apiarist',
+        'apiarist.script',
+        'apiarist.job',
+    ],
+    url='http://github.com/msharp/apiarist',
+    version=apiarist.__version__,
+    **setuptools_kwargs
+)
