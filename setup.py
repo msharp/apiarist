9c193cee5a4539e663b4ac975e4c5ab87f9e592b
Max Sharples <maxsharples@gmail.com>
2017-10-06 15:20:17 +1100

    minor version bump for Python 3.5 compat

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: System :: Distributed Computing',
     ],
     description='Python Hive query framework',
